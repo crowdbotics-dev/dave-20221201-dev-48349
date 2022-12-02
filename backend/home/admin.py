0ebc9c3016e48948bdabbc583263298f5a03fdf3
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-02 19:54:41 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Gfdsd
+admin.site.register(Gfdsd)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Gfdsd(models.Model):
+    'Generated Model'
+    asdfs = models.BigIntegerField()
